517ddc805ba30cde5ff2f523ec05ae8a45d55e8e
SimplyRobotic <maemaillols+pro@gmail.com>
2021-08-08 09:16:42 +1000

    Added death when out of lives. The game can now be lost.

--- PlayerDeath.cs
+++ PlayerDeath.cs
@@ -8,12 +8,23 @@ public class PlayerDeath : MonoBehaviour
 
     [SerializeField] Transform Respawn; // respawn point 
     public GameObject Player; // player 
+    public int startLives = 5; // starting lives
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Hazard"))
         {
             Player.transform.position = Respawn.position;
+            startLives = startLives - 1;
+        }
+
+        // Kills player if out of lives
+       if (startLives == 0)
+        {
+            Destroy(Player);
         }
     }
+
+
+
 }
