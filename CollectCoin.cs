2e03c36eea56355d05f0a49c8f65fabc4c963dfb
SimplyRobotic <67675511+SimplyRobotic@users.noreply.github.com>
2021-08-18 08:11:39 +1000

    Update CollectCoin.cs

--- CollectCoin.cs
+++ CollectCoin.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CollectCoin : MonoBehaviour
 {
     public GameObject Collectible;
+    public AudioSource collectSound;
 // Checks for collision and destroys collectable, then add 1 to the score
     void OnTriggerEnter2D(Collider2D other)
     {
 
         ScoringSystem.score++;        
         Destroy(Collectible);
+        collectSound.Play();
     }
 }
